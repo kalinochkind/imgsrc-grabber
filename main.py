67c3f046cebd75c7f20924a03f5e5aa6eb42b95c
Denis Kalinochkin <kalinochkind@gmail.com>
2017-03-04 17:16:14 +0300

    Обход с первой фотографии в альбоме

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import grab
 class ImgsrcParser:
 
     photo_re = re.compile(r"i='(\d+)',h=(?:i\.charAt\((\d)\)\+)?'(\d)?\.eu\.icdn\.ru',s='/',n='([^']{3})',u='([^']+)'")
+    prev_re = re.compile(r"\('left',function\(\) \{window\.location='([^']+)'")
     host = 'http://imgsrc.ru'
 
     def __init__(self, workdir):
@@ -16,17 +17,39 @@ class ImgsrcParser:
         self.g = grab.Grab()
         self.g.cookies.set('iamlegal', 'yeah', 'imgsrc.ru')
 
+    def normalize(self, url):
+        url = url.split('#')[0]
+        if url.startswith('/'):
+            url = self.host + url
+        return url
+
     def get_photos(self, url):
         res = []
+        url = self.first_photo(url)
         while not '/user.php' in url:
-            url = url.split('#')[0]
-            if url.startswith('/'):
-                url = self.host + url
+            url = self.normalize(url)
             print('Visiting', url)
             d = self.g.go(url)
             self.download_photo(self.get_photo_url(d.body.decode('utf-8')))
             url = d.tree.get_element_by_id('next_url').get('href')
         print('Finished')
+
+    def first_photo(self, url):
+        while True:
+            url = self.normalize(url)
+            d = self.g.go(url)
+            res = self.prev_re.search(d.body.decode('utf-8')).group(1)
+            if '/user.php' in res:
+                break
+            print('Previous page:', res)
+            url = res
+        d = self.g.go(url)
+        images = d.tree.xpath('//center/table/tr[@align="center"]/td//img')
+        if images[0].get('class') != 'cur':
+            ref = d.tree.xpath('//center/table/tr[@align="center"]/td/a')[0]
+            url = ref.get('href')
+            print('First photo:', url)
+        return url
 
     def get_photo_url(self, body):
         res = self.photo_re.search(body)
